0e870ab3a97b172b8979550cf4b61e066b3ae035
Presley McQuade <presleycosta@mpf.mp.br>
2024-10-16 22:21:09 -0300

    add imagem to card and rando card

--- src/components/MemroyGame.jsx
+++ src/components/MemroyGame.jsx
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { mirrorEasing, motion } from 'framer-motion';
+import  Card1 from '../assets/card_1.jpeg'; 
+import  Card2 from '../assets/card_2.jpeg';
+import  Card3 from '../assets/card_3.jpeg';
+import  Card4 from '../assets/card_4.jpeg';
+import  Card5 from '../assets/card_5.jpeg';
+import  Card6 from '../assets/card_6.jpeg';
+import  Card7 from '../assets/card_7.jpeg';
+import  Card8 from '../assets/card_8.jpeg';
+import  Card9 from '../assets/card_9.jpeg';
+import  Card10 from '../assets/card_10.jpeg';
+import  Card11 from '../assets/card_11.jpeg';
+import  Card12 from '../assets/card_12.jpeg';
+import  Card13 from '../assets/card_13.jpeg';
+import  Card14 from '../assets/card_14.jpeg';
+import  Card15 from '../assets/card_15.jpeg';
+import  Card16 from '../assets/card_16.jpeg';
+import  Card17 from '../assets/card_17.jpeg';
+import  Card18 from '../assets/card_18.jpeg';
 
-const cardImages = [
-  '🍎', '🍌', '🍉', '🍇', '🍓', '🍒', 
-  '🍎', '🍌', '🍉', '🍇', '🍓', '🍒'
-];
+const cardImagesDisponiveis = [Card1, Card2, Card3, Card4, Card5, Card6, Card7, Card8, Card9, Card10, Card11, Card12, Card13, Card14, Card15, Card16, Card17, Card18]
+
+
+const getRandomElements = (arr, count) => {
+  if (count > arr.length) {
+      throw new Error("O número de elementos solicitados é maior do que o tamanho do array.");
+  }
+  
+  const shuffledArray = arr.sort(() => 0.5 - Math.random()); // Embaralha o array
+  return shuffledArray.slice(0, count); // Retorna os primeiros 6 elementos
+}
+const getImages2Play = () => {
+  const randomCardImages = getRandomElements(cardImagesDisponiveis, 6);
+  return randomCardImages.concat(randomCardImages);
+}
+
 
 const shuffleCards = (array) => array.sort(() => Math.random() - 0.5);
 
@@ -17,7 +47,7 @@ const MemoryGame = () => {
   const [gameEnded, setGameEnded] = useState(false);
 
   useEffect(() => {
-    setCards(shuffleCards(cardImages));
+    setCards(shuffleCards(getImages2Play()));
   }, []);
 
   const handleCardClick = (index) => {
@@ -49,7 +79,7 @@ const MemoryGame = () => {
   };
 
   const handleRestartGame = () => {
-    setCards(shuffleCards(cardImages));
+    setCards(shuffleCards(getImages2Play()));
     setFlippedCards([]);
     setMatchedCards([]);
     setGameEnded(false);
@@ -58,7 +88,7 @@ const MemoryGame = () => {
   };
 
   return (
-    <div style={{ padding: '20px', maxWidth: '600px', margin: '0 auto' }}>
+    <div style={{ padding: '20px', maxWidth: '450px', margin: '0 auto' }}>
       <h2>Memórias de Itaewon</h2>
       <p>Vez de: <strong>{players[currentPlayerIndex].name}</strong></p>
       <span style={{ fontSize: '3rem'}}>{players[0].score} x {players[1].score}</span>
@@ -103,7 +133,11 @@ const MemoryGame = () => {
               animate={{ rotateY: isFlipped ? 180 : 0 }}
               transition={{ duration: 0.5 }}
             >
-              {isFlipped ? card : '❓'}
+              {isFlipped ? (
+                <img src={card} alt="card" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+              ) : (
+                <span style={{ fontSize: '24px', color: '#fff' }}>❓</span>
+              )}
             </motion.div>
           );
         })}
